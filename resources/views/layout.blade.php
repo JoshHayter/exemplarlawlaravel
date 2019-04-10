4335acf8a766478cd865c830c6b6e8926816a22b
Josh Hayter <josh@joshhayter.co.uk>
2019-04-10 13:37:26 +0100

    -Added bulma -Added some scss -Added layout and first welcome page with dependencies

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title', 'Exemplar Law')</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script defer src="https://use.fontawesome.com/releases/v5.8.1/js/all.js" integrity="sha384-g5uSoOSBd7KkhAMlnQILrecXvzst9TdC09/VM+pjDTCM+1il8RHz5fKANTFFb+gQ" crossorigin="anonymous"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
+  </head>
+  <body>
+    @yield('content')
+  </body>
+</html>
