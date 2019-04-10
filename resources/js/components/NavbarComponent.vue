db55453ea7dec3b70c8660585a689aa6ddc444e6
Josh Hayter <josh@joshhayter.co.uk>
2019-04-10 14:12:04 +0100

    -Added images -Added navbar component -Added logo to navbar

---
+++ resources/js/components/NavbarComponent.vue
@@ -0,0 +1,46 @@
+<template>
+<nav class="navbar">
+    <div class="navbar-brand">
+        <a class="navbar-item" href="/">
+            <img :src="inlineLogo" >
+        </a>
+
+        <a class="navbar-burger burger">
+            <span></span>
+            <span></span>
+            <span></span>
+        </a>
+    </div>
+
+    <div class="navbar-menu">
+        <div class="navbar-end">
+            <a class="navbar-item">
+                why
+            </a>
+
+            <a class="navbar-item">
+                who
+            </a>
+
+            <a class="navbar-item">
+                contact
+            </a>
+
+
+        </div>
+    </div>
+</nav>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log('NavbarComponent mounted.')
+    },
+    data: function() {
+        return {
+            inlineLogo: '/img/inlineLogo.png',
+        }
+    },
+}
+</script>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,10 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title', 'Exemplar Law')</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script defer src="{{ asset('js/app.js') }}"></script>
     <script defer src="https://use.fontawesome.com/releases/v5.8.1/js/all.js" integrity="sha384-g5uSoOSBd7KkhAMlnQILrecXvzst9TdC09/VM+pjDTCM+1il8RHz5fKANTFFb+gQ" crossorigin="anonymous"></script>
-    <script src="{{ asset('js/app.js') }}"></script>
   </head>
   <body>
-    @yield('content')
+    <div id='app'>
+      <navbar-component></navbar-component>
+      @yield('content')
+    </div>
   </body>
 </html>
